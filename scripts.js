4693184267e5860373367d067a13b7ab22b626aa
Bensangwa <36333986+Bensangwa@users.noreply.github.com>
2018-02-12 10:26:32 +0000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+    $(".sub-title").on("click", function(){
+        $(".call-to-action").slideToggle();
+    });
+
+    $(".call-button").on("click", function(e){
+        e.preventDefault();
+        $(".column img").toggleClass("bigger");
+    });
+
+    
+
+});
